495d46f2e115d51c4d57209b4b4d6f35fcc6437f
Roman-Isko <roman_isko@ukr.net>
2025-09-21 22:27:42 +0300

    new fixed

--- app/(private routes)/notes/filter/[...slug]/page.tsx
+++ app/(private routes)/notes/filter/[...slug]/page.tsx
@@ -38,15 +38,13 @@ import NotesClient from "../../../../../components/Notes/Notes.client";
 import type { NotesResponse } from "../../../../../types/note";
 import { getNotesServer } from "../../../../../lib/api/serverApi";
 
-interface NotesPageProps {
-  params: { slug?: string[] };
-  searchParams?: { page?: string; search?: string };
-}
-
 export default async function NotesPage({
   params,
   searchParams,
-}: NotesPageProps) {
+}: {
+  params: { slug?: string[] };
+  searchParams?: { page?: string; search?: string };
+}) {
   const { slug } = params;
   const page = searchParams?.page ?? "1";
   const search = searchParams?.search ?? "";
@@ -69,35 +67,3 @@ export default async function NotesPage({
     />
   );
 }
-
-// // app/(private routes)/notes/filter/[...slug]/page.tsx
-// import NotesClient from "../../../../../components/Notes/Notes.client";
-// import type { NotesResponse } from "../../../../../types/note";
-// import type { PageProps } from "next";
-// import { getNotesServer } from "../../../../../lib/api/serverApi";
-
-// export default async function NotesPage({
-//   params,
-//   searchParams,
-// }: PageProps<{ slug?: string[] }, { page?: string; search?: string }>) {
-//   const { slug } = params;
-//   const { page = "1", search = "" } = searchParams ?? {};
-
-//   const tag = slug?.[0] ?? "All";
-//   const pageNumber = Number(page) || 1;
-
-//   const initialNotes: NotesResponse = await getNotesServer({
-//     page: pageNumber,
-//     ...(search ? { search } : {}),
-//     ...(tag !== "All" ? { tag } : {}),
-//   });
-
-//   return (
-//     <NotesClient
-//       initialNotes={initialNotes}
-//       initialTag={tag}
-//       initialPage={pageNumber}
-//       initialSearch={search}
-//     />
-//   );
-// }
